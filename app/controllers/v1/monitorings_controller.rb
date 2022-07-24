216b6f28ecd7408cc6ab4b6eff5e22677254712d
whitia <whitia7@gmail.com>
2022-07-24 11:14:38 +0900

    Add monitoring resources

---
+++ app/controllers/v1/monitorings_controller.rb
@@ -0,0 +1,13 @@
+class V1::MonitoringsController < ApplicationController
+  def health
+    render json: { status: '200 OK' }, status: :ok
+  end
+
+  def version
+    render json: { version: '0.0.1' }, status: :ok
+  end
+
+  def metrics
+    render json: { metrics: nil }, status: :ok
+  end
+end
