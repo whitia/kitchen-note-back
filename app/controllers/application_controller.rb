8861dfdb925612364d8c8c5d4ab9b0537ac80ef9
whitia <whitia7@gmail.com>
2022-07-24 11:34:36 +0900

    Add before actions getting default params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :offset
+  before_action :limit
+
+  def offset
+    @offset = params[:offset].to_i
+  end
+
+  def limit
+    @limit = params[:limit].to_i
+  end
 end
