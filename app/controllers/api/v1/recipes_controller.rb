130112bd93f399c75388f7ef3a9ff783510de804
whitia <whitia7@gmail.com>
2022-07-25 00:35:49 +0900

    Update recipe actions

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -3,42 +3,47 @@ class API::V1::RecipesController < ApplicationController
 
   def index
     recipes = Recipe.order(created_at: :desc)
-    render json: recipes.to_json
+    render json: { status: 'SUCCESS', body: recipes.to_json, total: recipes.size }
   end
 
   def show
-    render json: @recipe.to_json
+    render json: { status: 'SUCCESS', body: @recipe.to_json }
   end
 
   def create
     recipe = Recipe.new(recipe_params)
     if recipe.save
-      render json: { status: 'SUCCESS', data: recipe }
+      render json: { status: 'SUCCESS', body: recipe }
     else
-      render json: { status: 'ERROR', data: recipe.errors }
+      render json: { status: 'ERROR', body: recipe.errors }
     end
   end
 
   def update
     if @recipe.update(recipe_params)
-      render json: { status: 'SUCCESS', data: @recipe }
+      render json: { status: 'SUCCESS', body: @recipe }
     else
-      render json: { status: 'ERROR', data: @recipe.errors }
+      render json: { status: 'ERROR', body: @recipe.errors }
     end
   end
 
   def destroy
     @recipe.destroy
-    render json: { status: 'SUCCESS', data: @recipe }
+    render json: { status: 'SUCCESS', body: @recipe }
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :path, :url)
+    params.require(:recipe).permit(
+      :title,
+      :uuid,
+      :external_title,
+      :external_url,
+    )
   end
 
   def set_recipe
-    @recipe = Recipe.find_by(path: params[:id])
+    @recipe = Recipe.find_by(uuid: params[:id])
   end
 end
