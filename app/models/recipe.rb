74eb0bd873873bd9bc0ebe24c0ee6a95aa8bbd60
whitia <whitia7@gmail.com>
2022-07-25 22:59:56 +0900

    Add validations to attributes of recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
   has_and_belongs_to_many :ingredients
+
+  validates :title, presence: true
+  validates :uuid, presence: true, uniqueness: true
+  validates :external_title, presence: true
+  validates :external_url, presence: true, uniqueness: true
 end
