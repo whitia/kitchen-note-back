843398bb15da98fc8e45038a25e45532b0391b16
whitia <whitia7@gmail.com>
2022-07-30 17:39:41 +0900

    Add image column into recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,19 @@
 class Recipe < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   has_and_belongs_to_many :ingredients
+
+  has_one_attached :image
 
   validates :title, presence: true
   validates :uuid, presence: true, uniqueness: true
   validates :external_title, presence: true
   validates :external_url, presence: true, uniqueness: true
+  validates :image, presence: true
+
+  def image_url
+    image.attached? ? url_for(image) : nil
+  end
 
   def save_ingredients(ingredients)
     current_ingredients = self.ingredients&.pluck(:name)
