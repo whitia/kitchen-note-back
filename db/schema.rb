879e424c9bf2c47a900f88c6e73cabd13cf05555
whitia <whitia7@gmail.com>
2022-08-03 07:27:13 +0900

    Add category column to recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_131927) do
+ActiveRecord::Schema.define(version: 2022_08_02_222618) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2022_07_28_131927) do
     t.string "external_url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
     t.index ["external_url"], name: "index_recipes_on_external_url", unique: true
     t.index ["uuid"], name: "index_recipes_on_uuid", unique: true
   end
