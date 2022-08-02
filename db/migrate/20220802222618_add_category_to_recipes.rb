d9d9672e328a63026de7fb24a1f416a8d658bad8
whitia <whitia7@gmail.com>
2022-08-03 07:54:42 +0900

    Add category column to recipes table

--- db/migrate/20220802222618_add_category_to_recipes.rb
+++ db/migrate/20220802222618_add_category_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddCategoryToRecipes < ActiveRecord::Migration[6.1]
   def change
-    add_column :recipes, :category, :string, null: false, default: ''
+    add_column :recipes, :category, :string
   end
 end
